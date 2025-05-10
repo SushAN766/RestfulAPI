1e597658be4e5103db41262ed48070188ac4290c
SushAN766 <sushanthbs766@gmail.com>
2025-05-11 00:14:09 +0530

    Refactor: merged APIs into unified structure

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const numbersRoute = require('./routes/numbers');
 const productRoutes = require('./routes/products');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use('/numbers', numbersRoute);
 app.use('/products', productRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = app;
